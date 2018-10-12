cffdcc19d25acdded662f02b262b07d0ff45dee2
James Tobey <jtobey@ssicorp.socialsolutions.com>
2018-10-12 18:54:08 -0500

    new spotify code doe

--- liri.js
+++ liri.js
@@ -2,13 +2,15 @@ require("dotenv").config();
 var request = require("request");
 var Spotify = require("node-spotify-api")
 const keys = require("./keys.js");
-const util = require('util')
+var fs = require('file-system');
 var bandsintown = require('bandsintown')('codingbootcamp')
 var spotify = new Spotify(keys.spotify);
 var moment = require('moment');
 moment().format();
 let artist = ""
 let movieName = ""
+let trackName = ""
+let randomSong=""
 arguments = process.argv.slice(2)
 if (arguments.length === 0) {
     console.log("Sorry, no command was given. Please try again.The commands you can choose from are:");
@@ -21,7 +23,7 @@ if (arguments.length === 0) {
     console.log("\r\n");
     console.log("do-what-it-says");
 }
-else if(arguments[0] !=="concert-this" && arguments[0] !=="movie-this" && arguments[0] !=="spotify-this-song" && arguments[0] !=="do-what-it-says" ){
+else if (arguments[0] !== "concert-this" && arguments[0] !== "movie-this" && arguments[0] !== "spotify-this-song" && arguments[0] !== "do-what-it-says") {
     console.log("Not a recognized command. Please try again.The commands you can choose from are:");
     console.log("\r\n");
     console.log("concert-this '<artist/band name here>'");
@@ -51,7 +53,7 @@ else if (arguments[0] === "concert-this") {
     bandsintown
         .getArtistEventList(artist)
         .then(function (response) {
-            
+
             if (response.length === 0) { console.log("No upcoming dates for this artist. Sorry! Maybe check another artist?") }
             else {
                 console.log("\n" + "Concert Information for " + artist + '\n')
@@ -65,14 +67,39 @@ else if (arguments[0] === "concert-this") {
 
 
 }
-else if (arguments[0] === "spotify-this-song") {
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        console.log(util.inspect(data, { showHidden: false, depth: null }))
+else if (arguments[0] === "spotify-this-song" ) {
+    for (var i = 1; i < arguments.length; i++) {
 
-    });
+        if (i > 1 && i < arguments.length) {
+
+            trackName = trackName + " " + arguments[i];
+
+        }
+
+        else {
+
+            trackName += arguments[i];
+
+        }}
+        spotify.search({ type: 'track', query: trackName }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            /*console.log(util.inspect(data, { showHidden: false, depth: null }))*/
+            
+            for (var i=0; i<data.tracks.items.length; i++){
+            if( data.tracks.items[i].preview_url !=null){
+            console.log(data.tracks.items[i].preview_url)
+            console.log(data.tracks.items[i].artists[0].name)
+            console.log(data.tracks.items[i].name)
+            console.log(data.tracks.items[i].album.name)
+            break
+            }
+            
+            }
+            
+        });
+    
 }
 else if (arguments[0] === "movie-this") {
     for (var i = 1; i < arguments.length; i++) {
@@ -88,14 +115,14 @@ else if (arguments[0] === "movie-this") {
             movieName += arguments[i];
 
         }
-    var urlOMDB = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    request(urlOMDB, function (error, response, body) {
-        var pBody=JSON.parse(body)
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-           
-            console.log("\n"+
-` Title : ${pBody.Title}
+        var urlOMDB = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+        request(urlOMDB, function (error, response, body) {
+            var pBody = JSON.parse(body)
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+
+                console.log("\n" +
+                    ` Title : ${pBody.Title}
  Year: ${pBody.Year}
  IMDB Rating: ${pBody.Ratings[0].Value}
  Rotten Tomatoes Rating: ${pBody.Ratings[1].Value}
@@ -103,13 +130,27 @@ else if (arguments[0] === "movie-this") {
  Language: ${pBody.Language}
  Plot: ${pBody.Plot}
  Actors:${pBody.Actors}`)
-        }
-    });
+            }
+        });
     }
 
 
 
 }
 else if (arguments[0] === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", (err, data) => {
+        if (err) throw err;
+        else {
+            var dataArr = data.split(",");
+            console.log(dataArr)
+            spotify.search({ type: 'track', query: dataArr[1] }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                
+                console.log(JSON.stringify(data, undefined, 2))
+            });
+        }
+    });
 
 }
